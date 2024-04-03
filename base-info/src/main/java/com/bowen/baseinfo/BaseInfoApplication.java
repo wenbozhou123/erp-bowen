b3460c07c12990badd095e52db4bb1fa6fe9efaa
zhoubowen <wenbozhou520@gmail.com>
2024-04-03 17:16:35 +0800

    first commit

---
+++ base-info/src/main/java/com/bowen/baseinfo/BaseInfoApplication.java
@@ -0,0 +1,13 @@
+package com.bowen.baseinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseInfoApplication.class, args);
+    }
+
+}
---
+++ base-info/src/test/java/com/bowen/baseinfo/BaseInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowen.baseinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
