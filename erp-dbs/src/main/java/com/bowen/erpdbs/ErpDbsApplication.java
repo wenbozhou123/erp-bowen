164d23bebb1ba6fb5f5e1e81abc59cf0ba9664e1
zhoubowen <wenbozhou520@gmail.com>
2024-04-07 11:01:23 +0800

    增加baseInfo 第一个DB脚本

---
+++ erp-dbs/src/main/java/com/bowen/erpdbs/ErpDbsApplication.java
@@ -0,0 +1,13 @@
+package com.bowen.erpdbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpDbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpDbsApplication.class, args);
+    }
+
+}
---
+++ erp-dbs/src/main/resources/dbs/baseInfo/商品表-product_info.sql
@@ -0,0 +1,3 @@
+create table product_info(
+
+);
