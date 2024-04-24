6f4d130e275f2fe6d8eb9e66eb7e509e054e4f6e
zhoubowen <wenbozhou520@gmail.com>
2024-04-24 15:44:11 +0800

    add idm

---
+++ erp-idm/src/main/java/com/bowen/erpidm/ErpIdmApplication.java
@@ -0,0 +1,13 @@
+package com.bowen.erpidm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpIdmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpIdmApplication.class, args);
+    }
+
+}
---
+++ erp-idm/src/test/java/com/bowen/erpidm/ErpIdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowen.erpidm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpIdmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
